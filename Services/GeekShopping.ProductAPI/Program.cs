99343ea82ae85d252879fa76821a9cbc1689b8fe
immerfallen <marodemelo10@gmail.com>
2024-01-20 09:49:58 -0300

    Criada entidade product e aplicada primeira migration

--- Services/GeekShopping.ProductAPI/Program.cs
+++ Services/GeekShopping.ProductAPI/Program.cs
@@ -1,8 +1,16 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+var connection = builder.Configuration["SqlConnection:SqlConnectionString"];
+builder.Services.AddDbContext<SQLServerContext>(options => options.UseSqlServer(connection));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
