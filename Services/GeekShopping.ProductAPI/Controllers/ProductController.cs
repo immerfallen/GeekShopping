345576364e0beec51ea68ab3544b21923ad8b5d3
immerfallen <marodemelo10@gmail.com>
2024-01-20 11:12:54 -0300

    Implementado controller de get e get by id

---
+++ Services/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Repository.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.ProductAPI.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ?? throw new
+                ArgumentNullException(nameof(productRepository));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ProductVO>>> FindAll()
+        {
+            var products = await _productRepository.FindAll();
+            if (products == null) { return NotFound(); };
+            return Ok(products);
+
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductVO>> FindById(long id)
+        {
+            var product = await _productRepository.FindById(id);
+            if(product == null) { return NotFound(); };
+            return Ok(product);
+
+
+        }
+    }
+}
