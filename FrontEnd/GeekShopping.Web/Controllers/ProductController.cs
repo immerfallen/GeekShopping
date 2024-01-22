dc8b3c77fd1c14386b20f45b3976a71c7b2560a1
immerfallen <marodemelo10@gmail.com>
2024-01-22 11:13:28 -0300

    Criado controller para listagem de produtos

---
+++ FrontEnd/GeekShopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.Xml;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService ?? throw new ArgumentNullException(nameof(productService));
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+            IEnumerable<ProductModel> products = await _productService.FindAllProducts();
+            return View(products);
+        }
+    }
+}
