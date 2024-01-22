9449513e5f7a5690cfb2df9f69b9c66c8e8dc828
immerfallen <marodemelo10@gmail.com>
2024-01-22 09:20:32 -0300

    Aplicando seed

--- Services/GeekShopping.ProductAPI/Migrations/20240122121801_Seed.Designer.cs
+++ Services/GeekShopping.ProductAPI/Migrations/20240122121801_Seed.Designer.cs
@@ -3,6 +3,7 @@ using GeekShopping.ProductAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeekShopping.ProductAPI.Migrations
 {
     [DbContext(typeof(SQLServerContext))]
-    partial class SQLServerContextModelSnapshot : ModelSnapshot
+    [Migration("20240122121801_Seed")]
+    partial class Seed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
