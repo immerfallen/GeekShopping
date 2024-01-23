ab7cd12ff6f28e9eddea2ebe06048b5db3f52c8d
immerfallen <marodemelo10@gmail.com>
2024-01-23 14:25:13 -0300

    Criadas classes para cart

---
+++ GeekShopping.CartAPI/Model/CartDetail.cs
@@ -0,0 +1,18 @@
+﻿using GeekShopping.CartAPI.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.CartAPI.Model
+{
+    [Table("cart_detail")]
+    public class CartDetail : BaseEntity
+    {
+        [ForeignKey("CartHeaderId")]
+        public CartHeader CartHeader { get; set; }
+        public long CartHeaderId { get; set; }       
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }       
+        public long ProductId { get; set; }
+        [Column("count")]
+        public int Count { get; set; }
+    }
+}
--- GeekShopping.CartAPI/Model/Product.cs
+++ GeekShopping.CartAPI/Model/Product.cs
@@ -5,8 +5,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace GeekShopping.CartAPI.Model
 {
     [Table("product")]
-    public class Product : BaseEntity
+    public class Product 
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Column("id")]
+        public long id { get; set; }
         [Column("name")]
         [Required]
         [StringLength(150)]
