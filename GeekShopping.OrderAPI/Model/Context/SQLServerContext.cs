37d86fdd312db5b23f3e8f65e24b509d639c97fa
immerfallen <marodemelo10@gmail.com>
2024-01-26 16:22:53 -0300

    Configurada startup e dbcontext do orderapi

---
+++ GeekShopping.OrderAPI/Model/Context/SQLServerContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.OrderAPI.Model.Context
+{
+    public class SQLServerContext : DbContext
+    {
+        
+        public SQLServerContext() { }           
+        public SQLServerContext(DbContextOptions<SQLServerContext> options) : base(options) { }        
+        public DbSet<OrderDetail> Details { get; set; }
+        public DbSet<OrderHeader> Headers { get; set; }
+        
+
+    }
+}
---
+++ GeekShopping.OrderAPI/Model/OrderDetail.cs
@@ -0,0 +1,26 @@
+﻿using GeekShopping.OrderAPI.Model.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.OrderAPI.Model
+{
+    [Table("order_detail")]
+    public class OrderDetail : BaseEntity
+    {
+        [ForeignKey("CartHeaderId")]
+        public virtual OrderHeader OrderHeader { get; set; }
+        public long CartHeaderId { get; set; }       
+        [ForeignKey("ProductId")]
+        public virtual Product Product { get; set; }       
+        public long ProductId { get; set; }
+        [Column("count")]
+        public int Count { get; set; }
+
+        public long Id { get; set; }
+        public long CartHeaderId { get; set; }
+        public CartHeaderVO CartHeader { get; set; }
+        public long ProductId { get; set; }
+        public ProductVO Product { get; set; }
+
+        public int Count { get; set; }
+    }
+}
