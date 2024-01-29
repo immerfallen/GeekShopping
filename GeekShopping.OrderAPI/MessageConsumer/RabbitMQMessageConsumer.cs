8db0ca8d4a0d731a6213c9b9581bdb72e1cba452
immerfallen <marodemelo10@gmail.com>
2024-01-29 11:45:10 -0300

    Implementaod consumidor da mensagem

--- GeekShopping.OrderAPI/MessageConsumer/RabbitMQMessageConsumer.cs
+++ GeekShopping.OrderAPI/MessageConsumer/RabbitMQMessageConsumer.cs
@@ -1,7 +1,11 @@
 ﻿
+using GeekShopping.OrderAPI.Messages;
 using GeekShopping.OrderAPI.Repository;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
+using System.ComponentModel.Design;
+using System.Text;
+using System.Text.Json;
 
 namespace GeekShopping.OrderAPI.MessageConsumer
 {
@@ -27,6 +31,21 @@ namespace GeekShopping.OrderAPI.MessageConsumer
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += (channel, evt) =>
+            {
+                var content = Encoding.UTF8.GetString(evt.Body.ToArray());
+                CheckoutHeaderVO vo = JsonSerializer.Deserialize<CheckoutHeaderVO>(content);
+                ProcessOrder(vo).GetAwaiter().GetResult();
+                _channel.BasicAck(evt.DeliveryTag, false);
+            };
+            _channel.BasicConsume("checkoutqueue", false, consumer);
+            return Task.CompletedTask;
+        }
+
+        private async Task ProcessOrder(CheckoutHeaderVO vo)
         {
             throw new NotImplementedException();
         }
