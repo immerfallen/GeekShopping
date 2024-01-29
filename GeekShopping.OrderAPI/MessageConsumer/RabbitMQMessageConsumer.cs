e0a762ad968ae09d8010513f6412aa443185fde7
immerfallen <marodemelo10@gmail.com>
2024-01-29 11:29:14 -0300

    Iniciada implementação do RabbitMQ consumer do order api

---
+++ GeekShopping.OrderAPI/MessageConsumer/RabbitMQMessageConsumer.cs
@@ -0,0 +1,34 @@
+﻿
+using GeekShopping.OrderAPI.Repository;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace GeekShopping.OrderAPI.MessageConsumer
+{
+    public class RabbitMQMessageConsumer : BackgroundService
+    {
+        private readonly OrderRepository _orderRepository;
+        private IConnection _connection;
+        private IModel _channel;
+
+        public RabbitMQMessageConsumer(OrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+            };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "checkoutqueue", false, false, false, arguments: null);
+           
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
